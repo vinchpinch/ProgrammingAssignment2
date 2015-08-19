769b7c00e2883a985af87bc414294a38df0deca1
Vincenzo <vsanlorenzo01@manhattan.edu>
2015-08-19 16:30:07 -0400

    Second Code
    I wrote the second code here

--- cachematrix.R
+++ cachematrix.R
@@ -23,6 +23,14 @@ list(set=set, get=get,setmatrix=setmatrix,getmatrix=getmatrix)
 ## What this function does is that it solves the matrix from the 
 # previous function. 
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x = matrix(), ...) {
+  m <- x$getmatrix()
+    if(!is.null(m)) {
+      message ("grabbing cached data")
+      return(m)
+    }
+  matrix <- x$get()
+  m <- solve(matrix, ...)
+  x$setmatrix(m)
+  m
 }
